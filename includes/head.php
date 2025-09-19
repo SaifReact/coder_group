ea073d4dc18f01fd1cdce5fd3a90add79383c819
SaifReact <saifur1985bd@gmail.com>
2025-09-19 23:39:41 +0600

    Update

--- includes/head.php
+++ includes/head.php
@@ -1,12 +1,12 @@
 <head>
     <meta charset="utf-8">
-    <title>iSTUDIO - Interior Design Website Template Free</title>
+    <title>Coder Group</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
 
     <!-- Favicon -->
-    <link href="/cpssl/new/assets/img/favicon.ico" rel="icon">
+    <link href="assets/img/favicon.ico" rel="icon">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -18,13 +18,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="/cpssl/new/assets/lib/animate/animate.min.css" rel="stylesheet">
-    <link href="/cpssl/new/assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="/cpssl/new/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="/cpssl/new/assets/css/style.css" rel="stylesheet">
-    <link href="/cpssl/new/assets/css/styles.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/styles.css" rel="stylesheet">
 </head>
