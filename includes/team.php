fcde75d90575e596c7fef16a165bc14eda9f964e
SaifReact <saifur1985bd@gmail.com>
2025-10-01 00:59:06 +0600

    Update

--- includes/team.php
+++ includes/team.php
@@ -5,10 +5,10 @@
             <div class="row g-4">
                 <div class="col-md-6 col-lg-2 wow fadeIn" data-wow-delay="0.1s">
                     <div class="team-item position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="assets/img/team-1.jpg" alt="">
+                        <img class="img-fluid w-100" src="assets/img/user.jpg" alt="">
                         <div class="team-overlay">
-                            <small class="mb-2">Architect</small>
-                            <h4 class="lh-base text-light">Boris Johnson</h4>
+                            <small class="mb-2">Pesident</small>
+                            <h4 class="lh-base text-light">&nbsp;</h4>
                             <div class="d-flex justify-content-center">
                                 <a class="btn btn-outline-primary btn-sm-square border-2 me-2" href="#!">
                                     <i class="fab fa-facebook-f"></i>
@@ -22,10 +22,10 @@
                 </div>
                 <div class="col-md-6 col-lg-2 wow fadeIn" data-wow-delay="0.3s">
                     <div class="team-item position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="assets/img/team-2.jpg" alt="">
+                        <img class="img-fluid w-100" src="assets/img/user.jpg" alt="">
                         <div class="team-overlay">
-                            <small class="mb-2">Architect</small>
-                            <h4 class="lh-base text-light">Donald Pakura</h4>
+                            <small class="mb-2">Vice President</small>
+                            <h4 class="lh-base text-light">&nbsp;</h4>
                             <div class="d-flex justify-content-center">
                                 <a class="btn btn-outline-primary btn-sm-square border-2 me-2" href="#!">
                                     <i class="fab fa-facebook-f"></i>
@@ -39,10 +39,10 @@
                 </div>
                 <div class="col-md-6 col-lg-2 wow fadeIn" data-wow-delay="0.5s">
                     <div class="team-item position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="assets/img/team-3.jpg" alt="">
+                        <img class="img-fluid w-100" src="assets/img/user.jpg" alt="">
                         <div class="team-overlay">
-                            <small class="mb-2">Architect</small>
-                            <h4 class="lh-base text-light">Bradley Gordon</h4>
+                            <small class="mb-2">Secretary</small>
+                            <h4 class="lh-base text-light">&nbsp;</h4>
                             <div class="d-flex justify-content-center">
                                 <a class="btn btn-outline-primary btn-sm-square border-2 me-2" href="#!">
                                     <i class="fab fa-facebook-f"></i>
@@ -56,10 +56,10 @@
                 </div>
                 <div class="col-md-6 col-lg-2 wow fadeIn" data-wow-delay="0.7s">
                     <div class="team-item position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="assets/img/team-4.jpg" alt="">
+                        <img class="img-fluid w-100" src="assets/img/user.jpg" alt="">
                         <div class="team-overlay">
-                            <small class="mb-2">Architect</small>
-                            <h4 class="lh-base text-light">Alexander Bell</h4>
+                            <small class="mb-2">Vice Secretary</small>
+                            <h4 class="lh-base text-light">&nbsp;</h4>
                             <div class="d-flex justify-content-center">
                                 <a class="btn btn-outline-primary btn-sm-square border-2 me-2" href="#!">
                                     <i class="fab fa-facebook-f"></i>
@@ -73,10 +73,10 @@
                 </div>
                 <div class="col-md-6 col-lg-2 wow fadeIn" data-wow-delay="0.7s">
                     <div class="team-item position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="assets/img/team-4.jpg" alt="">
+                        <img class="img-fluid w-100" src="assets/img/user.jpg" alt="">
                         <div class="team-overlay">
-                            <small class="mb-2">Architect</small>
-                            <h4 class="lh-base text-light">Alexander Bell</h4>
+                            <small class="mb-2">Treasurer</small>
+                            <h4 class="lh-base text-light">&nbsp;</h4>
                             <div class="d-flex justify-content-center">
                                 <a class="btn btn-outline-primary btn-sm-square border-2 me-2" href="#!">
                                     <i class="fab fa-facebook-f"></i>
@@ -90,10 +90,10 @@
                 </div>
                 <div class="col-md-6 col-lg-2 wow fadeIn" data-wow-delay="0.7s">
                     <div class="team-item position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="assets/img/team-4.jpg" alt="">
+                        <img class="img-fluid w-100" src="assets/img/user.jpg" alt="">
                         <div class="team-overlay">
-                            <small class="mb-2">Architect</small>
-                            <h4 class="lh-base text-light">Alexander Bell</h4>
+                            <small class="mb-2">Members</small>
+                            <h4 class="lh-base text-light">&nbsp;</h4>
                             <div class="d-flex justify-content-center">
                                 <a class="btn btn-outline-primary btn-sm-square border-2 me-2" href="#!">
                                     <i class="fab fa-facebook-f"></i>
